6b54a3bdb94c0fe88f95fe4f14556888252a302f
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2024-01-10 15:46:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const QuizData = require("./modules/QuesionAnswer");
+const path = require('path');
 const Data = express();
+Data.use(express.static('public')); 
 const port = 8080;
 
 QuizData.Initialize()
@@ -13,10 +15,13 @@ QuizData.Initialize()
     console.error('Initialization failed:', error);
   });
 
-  Data.get ('/',(req,res)=>{
-    res.send('Testing');
-});
-  
+  Data.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+  });
+
+  Data.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/about.html'));
+  });
 
 Data.get('/questions',(req,res)=>{
     QuizData.getAllQuestions()
