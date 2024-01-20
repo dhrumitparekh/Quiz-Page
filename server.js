f517c442e5ae5d38dca3fc24aa6df53bb28071ff
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2024-01-20 02:10:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const QuizData = require("./modules/QuesionAnswer");
 const path = require('path');
 const Data = express();
 Data.use(express.static('public')); 
+Data.set('view engine', 'ejs');
 const port = 8080;
 
 QuizData.Initialize()
@@ -16,37 +17,41 @@ QuizData.Initialize()
   });
 
   Data.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.render("home");
   });
 
 
   Data.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/about.html'));
+    res.render("about");
+  });
+  
+  Data.get('/questions', async (req, res) => {
+    const QuestType = req.query.type;
+    try {
+
+      if (QuestType) {
+        const quest = await QuizData.getQuestionsByType(QuestType);
+        res.render('mainpage', { questions: quest });  
+      } 
+      else {
+        const allQuestions = await QuizData.getAllQuestions();
+        res.render('mainpage', { questions: allQuestions });  
+      }
+    } catch (error) {
+      res.status(404).render('mainpage', { error: error.message });
+    }
   });
 
-Data.get('/questions',async(req,res)=>{
-  const QuestType = req.query.type;
-  try{
-    if(QuestType)
-    {
-      const quest = await QuizData.getQuestionsByType(QuestType);
-      res.json(quest);
-    }
-    else
-    {
-    QuizData.getAllQuestions()
-    .then((Data) => {
-        res.json(Data);
-      })
-    }
-    }
-    catch 
-    {
-      res.status(404).json({error:error});
-    }
-  });
-  
-  
+    Data.get("//",async (req, res) => {
+      try {
+        const Type = await QuizData.getAllTypes();
+        res.render("questpage", { Type });
+      } catch (err) {
+        res.render("404", { message: `Error: ${err.message}` });
+      }
+    });
+    
+
  
     Data.get('/questions/get_Question/:id', (req, res) => {
       const Questid = req.params.id; 
