b275f9d5dd02baac2a5cc40e047ee891148f9d03
dhrumitparekh <137636355+dhrumitparekh@users.noreply.github.com>
2024-01-26 01:20:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const QuizData = require("./modules/QuesionAnswer");
+const QuizData = require("./modules/QuestionAnswer");
 const path = require('path');
 const Data = express();
 Data.use(express.static('public')); 
+const bodyParser = require('body-parser');
+Data.use(express.urlencoded({ extended: true }));
 Data.set('view engine', 'ejs');
 const port = 8080;
 
+Data.use(bodyParser())
 QuizData.Initialize()
   .then(() => {
     Data.listen(port, () => {
@@ -31,11 +34,11 @@ QuizData.Initialize()
 
       if (QuestType) {
         const quest = await QuizData.getQuestionsByType(QuestType);
-        res.render('mainpage', { questions: quest });  
+        res.render('mainpage', { quest });  
       } 
       else {
         const allQuestions = await QuizData.getAllQuestions();
-        res.render('mainpage', { questions: allQuestions });  
+        res.render('mainpage', {  quest : allQuestions });  
       }
     } catch (error) {
       res.status(404).render('mainpage', { error: error.message });
@@ -51,16 +54,67 @@ QuizData.Initialize()
       }
     });
     
-
- 
     Data.get('/questions/get_Question/:id',async(req, res) => {
       const Questid = req.params.id; 
       try{
-        const specificquest = await QuizData.getQuestionsById(Questid);
-        res.render("questId",{questions:specificquest});
+        const questions = await QuizData.getQuestionsById(Questid);
+        res.render("questId",{questions});
       }catch(error){
-        res.render("404", { message: `Error: ${err.message}` });
+        res.render("404", { message: `Error: ${error.message}` });
         }
+    });
+    Data.get('/questions/deleteQuestions/:id', async (req, res) => {
+      try {
+        await QuizData.deleteQuestion(req.params.id);
+        res.redirect('/questions');
+      } catch (err) {
+        res.status(500).render('500', { message: `Error: ${err}` });
+      }
+    });
+  
+  
+    Data.get("/questions/addQuestion", async (req, res) => {
+      try {
+        const Types = await QuizData.getAllTypes();
+        res.render("addQuestions", { Types });
+      } catch (err) {
+        res.render("404", { message: `Error: ${err.message}` });
+      }
+    });
+
+    Data.post('/questions/addQuestion',async (req, res) => {
+      try {
+  
+        await QuizData.addQuestion(req.body);
+        res.redirect('/questions');
+      } catch (err) {
+        console.log(err);
+        res.render("404", { message: `Error: ${err}` });
+      }
+    });
+
+    Data.get('/questions/editQuestion/:id',async (req, res) => {
+      try {
+        const Quests = await QuizData.getQuestionsById(req.params.id);
+        const Types = await QuizData.getAllTypes();
+    
+        res.render('editQuestions', { Quests, Types });
+      } catch (err) {
+        res.status(404).render('404', { message: err.message });
+      }
+    });
+    
+    Data.post('/questions/editQuestion',async (req, res) => {
+      try {
+        const empId = req.body.id; 
+        const updatedData = req.body; 
+    
+        await QuizData.editQuestion(empId, updatedData);
+    
+        res.redirect('/questions');
+      } catch (err) {
+        res.status(404).render('404', { message: `Error: ${err}` });
+      }
     });
 
   
